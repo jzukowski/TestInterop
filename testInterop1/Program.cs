01a753936482bc3e0aed776d5cc32b4b3f563a7a
Jan Zukowski <Jan.Zukowski@vrx-hx37g3ngkh.myfiosgateway.com>
2024-06-10 16:25:06 -0400

    initial commit

---
+++ testInterop1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(example.get_time());
+Console.WriteLine(example.fact(5));
+Console.WriteLine(example.my_mod(6,2));
+Console.WriteLine(example.My_variable);
